099dcbdab13307b03e98f04b5427f5764cf72cc5
xpluna <76738691+xpluna@users.noreply.github.com>
2021-10-25 17:51:07 +0530

    Little shortening

--- collatz_sequence.rb
+++ collatz_sequence.rb
@@ -2,9 +2,9 @@ puts "Enter a number"
 n, moves = gets.chomp.to_i, 0
 
 while n != 1
-    n += (n % 2 == 0) ? -n/2 : 2*n+1
-    puts n
-    moves += 1
+	n += n.even? ? -n/2 : 2*n+1
+	puts n
+	moves += 1
 end
 
 puts "\nMoves count = #{moves}"
